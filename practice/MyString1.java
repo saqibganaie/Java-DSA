cf7f6f83e9e6af3621d7d4994ab44295ccea4b35
Saqibhussain596 <saqibhussain596@gmail.com>
2023-04-28 18:48:15 +0530

    Strings Lecture Completed

--- practice/MyString1.java
+++ practice/MyString1.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MyString1 {
     public static void main(String[] args) {
         // Strings are immutable
@@ -16,5 +19,26 @@ public class MyString1 {
         System.out.printf("Formatted number is %.2f%n", 21.22345); // %n is for new line
         System.out.printf("Pie : %.4f%n", Math.PI);
         System.out.printf("Hi! My name is %s and I am really %s%n", "Saqib", "Cool");
+
+        // * The + operator is overloaded in Java to evaluate strings too
+        // * we cannot overload the default operators in Java
+
+        System.out.println('a' + 'b'); // characters are converted to ascii code and then added while using the +
+                                       // operator
+        System.out.println("a" + "b"); // Strings are concatenated using + operator
+        System.out.println("a" + 1); // int will be converted to string
+        System.out.println((char) ('a' + 1)); // char will be converted to ascii and then added with int
+        int[] arr = { 1, 2, 3 };
+        System.out.println("a" + Arrays.toString(arr)); /*
+                                                         * we can also add complex expressions such arrays,
+                                                         * arrayLists and objects with the + operator but the only
+                                                         * condition is that the expression should have atleast one
+                                                         * string in it so that everything will be evaluated as string.
+                                                         */
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        System.out.println("abc" + list);
+
     }
 }
---
+++ practice/StringMethods3.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class stringMethods {
+    public static void main(String[] args) {
+        // Strings are immutable so these methods will not change the actual string but
+        // will return a new refrence.
+        String intro = "Hi my name is Saqib!";
+        char[] arr = intro.toCharArray();
+        System.out.println(Arrays.toString(arr));
+        System.out.println(intro.toLowerCase());
+        System.out.println(intro.indexOf("a"));
+        System.out.println("     Saqib     ".strip());
+        System.out.println(Arrays.toString(intro.split(" "))); // inside the split() we pass regular expressions (regEx)
+                                                               // to split the string according to our needs
+    }
+}
