d4bcb5510925978751ecf91cff4176d4c8d36a01
Saqib Hussain <saqibhussain596@gmail.com>
2024-01-01 12:13:58 +0530

    Hashing branch created

--- 04-oops/practice/Interfaces/Media_Interfaces5b.java
+++
@@ -1,6 +0,0 @@
-// package Interfaces;
-public interface Media_Interfaces5b {
-    void start();
-
-    void stop();
-}
--- 04-oops/practice/packages/one/Hello.java
+++
@@ -1,7 +0,0 @@
-package packages.one;
-
-public class Hello {
-    public static void hello() {
-        System.out.println("This is hello in package one");
-    }
-}
